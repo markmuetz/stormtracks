1ecc56995405e9fe734f185e4a56e07a289fd4f6
Mark Muetzelfeldt <markmuetz@gmail.com>
2014-07-02 15:37:34 +0100

    Allow for different computers to analyse different ensemble members.

--- ipython_setup.py
+++ ipython_setup.py
@@ -1,5 +1,6 @@
 import time
 import datetime as dt
+import socket
 
 import detect as d
 import load as l
@@ -12,12 +13,25 @@ num_ensemble_members = 3
 start = time.time()
 print(start)
 
+short_name = socket.gethostname().split('.')[0] 
+
+if short_name == 'linz':
+    ensemble_member_range = range(0, 3)
+elif short_name == 'athens':
+    ensemble_member_range = range(3, 6)
+elif short_name == 'madrid':
+    ensemble_member_range = range(6, 9)
+elif short_name == 'madrid':
+    ensemble_member_range = range(6, 9)
+elif short_name == 'determinist-mint':
+    ensemble_member_range = range(9, 12)
+
 tracks, cou = l.load_ibtracks_year(2005)
-ncdata = d.NCData(2005)
+ncdata = d.NCData(2005, verbose=False)
 gdatas = []
 all_good_matches = []
 
-for i in range(num_ensemble_members):
+for i in ensemble_member_range:
     gdata = d.GlobalCyclones(ncdata, i)
     #gdata.track_vort_maxima(dt.datetime(2005, 6, 1), dt.datetime(2005, 7, 1))
     gdata.track_vort_maxima(dt.datetime(2005, 6, 1), dt.datetime(2005, 12, 1))
@@ -28,4 +42,5 @@ for i in range(num_ensemble_members):
 
 end = time.time()
 
+print('{0} - {1}'.format(short_name, ensemble_member_range))
 print('Start: {0}, end: {1}, duration: {2}'.format(start, end, end - start))
