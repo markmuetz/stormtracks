016b1d4e6dc1acbd9a11f43a2cdad4c1787c5ce7
Mark Muetzelfeldt <markmuetz@gmail.com>
2014-07-09 16:20:13 +0100

    Add new default settings.

--- stormtracks/settings/default.py
+++ stormtracks/settings/default.py
@@ -3,3 +3,7 @@ import os
 # expandvars expands e.g. $HOME
 DATA_DIR = os.path.expandvars('$HOME/stormtracks/data')
 OUTPUT_DIR = os.path.expandvars('$HOME/stormtracks/output')
+
+C20_FULL_DATA_DIR = os.path.join(DATA_DIR, 'c20_full')
+C20_MEAN_DATA_DIR = os.path.join(DATA_DIR, 'c20_mean')
+IBTRACS_DATA_DIR = os.path.join(DATA_DIR, 'ibtracs')
