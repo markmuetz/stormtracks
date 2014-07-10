29d556c6632c46e1ed32198fd18aa3f6c6293262
Mark Muetzelfeldt <markmuetz@gmail.com>
2014-07-10 11:12:14 +0100

    Turn into an installable python project by including a setup.py.
    Begin adding some documentation in the form of a README.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup, Extension
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+      name='stormtracks',
+      version='0.1',
+      description='Tropical Cyclone Detection and Tracking',
+      long_description=read('README.rst'),
+      author='Mark Muetzelfeldt',
+      author_email='markmuetz@gmail.com',
+
+      packages=['stormtracks', 'stormtracks.settings', 
+                'stormtracks.utils', 'stormtracks.pyro_cluster',
+                'stormtracks.demo'],
+      ext_modules=[Extension('stormtracks', ['src/cvort.c', 'src/cextrema.c'])],
+      requires=['numpy', 'scipy', 'pylab', 'mpl_toolkits.Basemap', 'netCDF4'],
+      data_files=[('stormtracks/shell_scipts', ['stormtracks/shell_scripts/kill_pyro.sh'])],
+
+      url = 'https://github.com/markmuetz/stormtracks',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: Public Domain',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7'
+          'Programming Language :: C',
+          'Topic :: Scientific/Engineering :: Atmospheric Science',
+          ],
+      keywords=['tropical cyclone track detection'],
+     )
--- stormtracks/utils/c_wrapper.py
+++ stormtracks/utils/c_wrapper.py
@@ -4,8 +4,10 @@ from numpy.ctypeslib import ndpointer
 
 local_dir = os.path.dirname(os.path.abspath(__file__))
 
-libcvort = ctypes.cdll.LoadLibrary(os.path.join(local_dir, "../../lib/libcvort.so"))
-cvort = libcvort.cvort
+# Find the library.
+stormtracks_lib = ctypes.cdll.LoadLibrary(os.path.join(local_dir, "../../stormtracks.so"))
+
+cvort = stormtracks_lib.cvort
 cvort.restype = None
 cvort.argtypes = [ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
                   ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
@@ -15,7 +17,7 @@ cvort.argtypes = [ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
 		  ctypes.c_float,
                   ndpointer(ctypes.c_float, flags="C_CONTIGUOUS")]
 
-cvort4 = libcvort.cvort4
+cvort4 = stormtracks_lib.cvort4
 cvort4.restype = None
 cvort4.argtypes = [ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
                    ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
@@ -25,7 +27,7 @@ cvort4.argtypes = [ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
 		   ctypes.c_float,
                    ndpointer(ctypes.c_float, flags="C_CONTIGUOUS")]
 
-cextrema = libcvort.cextrema
+cextrema = stormtracks_lib.cextrema
 cextrema.restype = None
 cextrema.argtypes = [ndpointer(ctypes.c_float, flags="C_CONTIGUOUS"),
                      ctypes.c_size_t,
