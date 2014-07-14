4bcd02921a762cfa6a96329ac5a0b3a54515c3cc
Mark Muetzelfeldt <markmuetz@gmail.com>
2014-07-14 16:39:20 +0100

    Release 0.1.4: plotting cleared up, basic testing in place.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
       name='stormtracks',
-      version='0.1.3',
+      version='0.1.4',
       description='Tropical Cyclone Detection and Tracking',
       long_description=read('README.rst'),
       author='Mark Muetzelfeldt',
