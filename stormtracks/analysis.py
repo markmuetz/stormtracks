1e1d8f0cc726c41d0c3093e93ed4c5ecf1dbe0df
Mark Muetzelfeldt <markmuetz@gmail.com>
2014-07-23 15:29:27 +0100

    Add a new analysis class that can use existing classes to perform analyses.
    It should be easier to configure/save state etc with the code in a
    class. It's already saving its data using a (modified) results manager.

---
+++ stormtracks/analysis.py
@@ -0,0 +1,84 @@
+import datetime as dt
+
+from results import StormtracksResultsManager
+from ibtracsdata import IbtracsData
+from c20data import C20Data, GlobalEnsembleMember
+from tracking import VortmaxFinder, VortmaxNearestNeighbourTracker, VortmaxKalmanFilterTracker
+from match import match
+
+
+class StormtracksAnalysis(object):
+    def __init__(self, year, ensemble_member):
+        self.year = year
+        self.ensemble_member = ensemble_member
+
+        self.ibdata = IbtracsData(verbose=False)
+        self.best_tracks = self.ibdata.load_ibtracks_year(year)
+        self.results = {}
+
+        self.c20data = C20Data(year, verbose=False)
+        self.results_manager = StormtracksResultsManager('analysis_')
+
+    def run_analysis(self):
+        scales = [1, 2, 3]
+        pressure_levels = [995, 850, 250]
+        trackers = ['nearest_neighbour', 'kalman']
+
+        for scale in scales:
+            for pressure_level in pressure_levels:
+                for tracker_name in trackers:
+                    result_key = 'scale:{0};pl:{1};tracker:{2}'.format(scale,
+                                                                       pressure_level,
+                                                                       tracker_name)
+                    saved_results = self.results_manager.list_results(self.year,
+                                                                      self.ensemble_member)
+                    if result_key not in saved_results:
+                        print('Running analysis: {0}'.format(result_key))
+                        result = self.run_individual_analysis(scale, pressure_level, tracker_name)
+                        self.results_manager.add_result(self.year,
+                                                        self.ensemble_member,
+                                                        result_key,
+                                                        result)
+                        self.results_manager.save()
+                    else:
+                        print('Loading saved result: {0}'.format(result_key))
+                        self.results_manager.load(self.year,
+                                                  self.ensemble_member,
+                                                  result_key)
+
+    def run_individual_analysis_from_results_key(self, key):
+        config = key.split(':')
+        scale = int(config[0].split(';')[1])
+        pressure_level = int(config[1].split(';')[1])
+        tracker_name = config[2].split(';')[1]
+        result = self.run_individual_analysis(scale, pressure_level, tracker_name)
+        return result
+
+    def run_individual_analysis(self, scale, pressure_level, tracker_name):
+        if scale == 1:
+            self.c20data.upscaling = False
+            self.c20data.scale_factor = scale
+        else:
+            print('Using upscaled')
+            self.c20data.upscaling = True
+            self.c20data.scale_factor = scale
+
+        self.c20data.pressure_level = pressure_level
+
+        if tracker_name == 'nearest_negighbour':
+            tracker = VortmaxNearestNeighbourTracker()
+        elif tracker_name == 'kalman':
+            tracker = VortmaxKalmanFilterTracker()
+
+        gem = GlobalEnsembleMember(self.c20data, self.ensemble_member)
+        vort_finder = VortmaxFinder(gem)
+
+        vort_finder.find_vort_maxima(dt.datetime(self.year, 6, 1), dt.datetime(self.year, 12, 1))
+
+        tracker = VortmaxNearestNeighbourTracker()
+        tracker.track_vort_maxima(vort_finder.vortmax_time_series)
+
+        matches = match(tracker.vort_tracks_by_date, self.best_tracks)
+        good_matches = [ma for ma in matches.values() if ma.av_dist() < 5 and ma.overlap > 6]
+
+        return good_matches
--- stormtracks/results.py
+++ stormtracks/results.py
@@ -6,10 +6,10 @@ from collections import OrderedDict
 
 from load_settings import settings
 
-RESULTS_TPL = 'results_em_{0}.pkl'
+RESULTS_TPL = '{0}-{1}-{2}.pkl'
 
 
-class StormtracksResult(object):
+class StormtracksResults(object):
     '''Utility class that is easier to use than a dict
 
     :param dct: dict used to populate this class' fields
@@ -27,8 +27,10 @@ class StormtracksResultsManager(object):
     (using that as a directory/filename structure).
     Saves each result as a name in a dictionary that then gets serialized to disk.
     '''
-    def __init__(self):
+    def __init__(self, name):
         self._results = OrderedDict()
+        self.name = name
+        self.saved = []
 
     def add_result(self, year, ensemble_member, name, result):
         '''Adds a given result based on year, ensemble_member and a user chosen name'''
@@ -42,16 +44,16 @@ class StormtracksResultsManager(object):
             raise Exception('Result {0} has already been added'.format(name))
         self._results[year][ensemble_member][name] = result
 
-    def get_result(self, year, ensemble_member):
+    def get_results(self, year, ensemble_member):
         '''Gets a set of results based on year, ensemble_member'''
         try:
-            result = StormtracksResult(self._results[year][ensemble_member])
-            return result
+            results = StormtracksResults(self._results[year][ensemble_member])
+            return results
         except KeyError:
             print('Could not find entry for {0}, {1}'.format(year, ensemble_member))
 
     def save(self):
-        '''Saves **all** results that have been added so far'''
+        '''Saves all unsaved results that have been added so far'''
         for year in self._results.keys():
             y = str(year)
             dirname = os.path.join(settings.OUTPUT_DIR, y)
@@ -59,31 +61,57 @@ class StormtracksResultsManager(object):
                 os.makedirs(dirname)
 
             for ensemble_member in self._results[year].keys():
-                f = open(os.path.join(dirname, RESULTS_TPL.format(ensemble_member)), 'w')
-                cPickle.dump(self._results[year][ensemble_member], f)
+                for results_key in self._results[year][ensemble_member].keys():
+                    if (year, ensemble_member, results_key) not in self.saved:
+                        filename = RESULTS_TPL.format(self.name, ensemble_member, results_key)
+                        print('saving {0}'.format(filename))
+                        f = open(os.path.join(dirname, filename), 'w')
+                        cPickle.dump([results_key,
+                                      self._results[year][ensemble_member][results_key]], f)
+                        self.saved.append((year, ensemble_member, results_key))
 
-    def load(self, year=2005, ensemble_member=0):
+    def load(self, year=2005, ensemble_member=0, result_key=None):
         '''Loads results from disk'''
         y = str(year)
         dirname = os.path.join(settings.OUTPUT_DIR, y)
         try:
-            f = open(os.path.join(dirname, RESULTS_TPL.format(ensemble_member)), 'r')
-            _results = cPickle.load(f)
-
-            for name, result in _results.items():
-                self.add_result(year, ensemble_member, name, result)
+            if not result_key:
+                filenames = sorted(glob(os.path.join(dirname, RESULTS_TPL.format(self.name,
+                                                                                 ensemble_member,
+                                                                                 '*'))))
+                for filename in filenames:
+                    print(filename)
+                    self._load_filename(year, ensemble_member, filename)
+            else:
+                filename = os.path.join(dirname, RESULTS_TPL.format(self.name,
+                                                                    ensemble_member,
+                                                                    result_key))
+                print(filename)
+                self._load_filename(year, ensemble_member, filename)
 
         except Exception, e:
             print('Results {0}, {1} could not be loaded'.format(year, ensemble_member))
             print('{0}'.format(e.message))
             raise e
 
+    def _load_filename(self, year, ensemble_member, filename):
+        f = open(filename, 'r')
+        key_and_result = cPickle.load(f)
+        key = key_and_result[0]
+        result = key_and_result[1]
+
+        self.add_result(year, ensemble_member, key, result)
+        self.saved.append((year, ensemble_member, key))
+
     def delete(self, year=2005, ensemble_member=0):
         '''Deletes a specific result from disk'''
+        raise Exception('Needs fixing')
         y = str(year)
         dirname = os.path.join(settings.OUTPUT_DIR, y)
         try:
-            os.remove(os.path.join(dirname, RESULTS_TPL.format(ensemble_member)))
+            os.remove(os.path.join(dirname, RESULTS_TPL.format(self.name,
+                                                               ensemble_member,
+                                                               ensemble_member)))
         except Exception, e:
             print('Results {0}, {1} could not be deleted'.format(year, ensemble_member))
             print('{0}'.format(e.message))
@@ -98,8 +126,16 @@ class StormtracksResultsManager(object):
         '''List all results saved for a particular year'''
         dirname = os.path.join(settings.OUTPUT_DIR, str(year))
         _results_names = []
-        for fn in glob(os.path.join(dirname, RESULTS_TPL.format('*'))):
-            _results_names.append('_'.join(os.path.basename(fn).split('.')[0].split('_')[1:]))
+        for fn in glob(os.path.join(dirname, RESULTS_TPL.format(self.name, '*', '*'))):
+            _results_names.append(os.path.basename(fn).split('.')[0])
+        return sorted(_results_names)
+
+    def list_results(self, year, ensemble_member):
+        '''List all results saved for a particular year'''
+        dirname = os.path.join(settings.OUTPUT_DIR, str(year))
+        _results_names = []
+        for fn in glob(os.path.join(dirname, RESULTS_TPL.format(self.name, ensemble_member, '*'))):
+            _results_names.append(os.path.basename(fn).split('.')[0].split('-')[-1])
         return sorted(_results_names)
 
     def list_years(self):
