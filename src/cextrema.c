b26a13bac5206cf20cf506dbdb05207648f21503
Mark Muetzelfeldt <markmuetz@gmail.com>
2015-07-30 11:57:41 +0100

    Try to improve cextrema to build array of max/mins.

--- src/cextrema.c
+++ src/cextrema.c
@@ -16,6 +16,9 @@ void cextrema(const float *data,
 
     float data_val;
 
+    int max_count;
+    int min_count;
+
     for (i = 1; i < imax - 1; ++i)
     {
         for (j = 1; j < jmax - 1; ++j)
@@ -42,11 +45,39 @@ void cextrema(const float *data,
             if (is_max)
             {
                 extrema[i * jmax + j] = 1;
+		max_count++;
             }
             else if (is_min)
             {
                 extrema[i * jmax + j] = -1;
+		min_count++;
             }
         }
+    }
+
+    int* maxima_x = (int*)malloc(max_count * sizeof(int));
+    int* maxima_y = (int*)malloc(max_count * sizeof(int));
+    int* minima_x = (int*)malloc(min_count * sizeof(int));
+    int* minima_y = (int*)malloc(min_count * sizeof(int));
+    int max_index = 0;
+    int min_index = 0;
+
+    for (i = 1; i < imax - 1; ++i)
+    {
+        for (j = 1; j < jmax - 1; ++j)
+        {
+	    if (extrema[i * jmax + j] == 1)
+	    {
+		maxima_x[max_index] = i;
+		maxima_x[max_index] = j;
+		max_index++;
+	    }
+	    if (extrema[i * jmax + j] == -1)
+	    {
+		minima_x[min_index] = i;
+		minima_x[min_index] = j;
+		min_index++;
+	    }
+	}
     }
 }
