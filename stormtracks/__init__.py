d1bdddcf72bc788e38582e39936cddbf4ca77fbb
Mark Muetzelfeldt <markmuetz@gmail.com>
2015-08-06 12:45:25 +0100

    Add a working __init__.__all__

--- stormtracks/__init__.py
+++ stormtracks/__init__.py
@@ -1,9 +1,15 @@
 from .version import __version__
-# from .c20data import FullC20Data
-# from .ibtracsdata import IbtracsData
-
-__all__ = [
-    '__version__',
-    # 'FullC20Data',
-    # 'IbtracsData',
-]
+try:
+    from .c20data import C20Data
+    from .ibtracsdata import IbtracsData
+    __all__ = [
+	'__version__',
+	'C20Data',
+	'IbtracsData',
+    ]
+except ImportError:
+    # This can be called on installation, when numpy et al. won't be installed.
+    # Handle import errors and just expose __version__.
+    __all__ = [
+	'__version__',
+    ]
